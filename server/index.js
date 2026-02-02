f1ee7f6cd3823f5d940af5ad44426bd366141b23
Spartan 253 <ayushkhoke1@gmail.com>
2026-02-03 00:06:20 +0530

    All errors are fixed

--- server/index.js
+++ server/index.js
@@ -91,7 +91,7 @@ const allowedOrigins = [
   "http://localhost:3000",
   "http://localhost:3001",
   // "https://studynotion3-ylbj.vercel.app",
-  "https://studynotion-online-platform-nzmx.vercel.app/"
+  "https://studynotion3-eight.vercel.app"
   
 ];
 
