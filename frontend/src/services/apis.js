603d0192b9f77248c3c8a5c05e75dd5ce9c4d497
Spartan 253 <ayushkhoke1@gmail.com>
2026-02-02 17:56:01 +0530

    Fix API base URL for production

--- frontend/src/services/apis.js
+++ frontend/src/services/apis.js
@@ -1,6 +1,6 @@
 // const BASE_URL = "https://studynotion3-3.onrender.com/api/v1";
 //https://studynotion-online-platform.onrender.com
-const BASE_URL = "http://localhost:4000/api/v1";
+const BASE_URL = "https://studynotion3-3.onrender.com/api/v1";
 export const categories={
     CATEGORIES_API:BASE_URL+"/course/showallCategory"
 }
