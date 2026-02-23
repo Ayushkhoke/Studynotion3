bc5e6f03d20f14d68e63b2e1ab540a1b02d04a21
Spartan 253 <ayushkhoke1@gmail.com>
2026-02-23 14:53:59 +0530

    Fixed CORS for production

--- server/utils/mailsennder.js
+++ server/utils/mailsennder.js
@@ -29,22 +29,54 @@
 // module.exports = mailsender;
 
 
+// const nodemailer = require("nodemailer");
+
+// const mailsender = async (email, title, body) => {
+//   try {
+//     const transporter = nodemailer.createTransport({
+//       host: process.env.MAIL_HOST,
+//       port: process.env.MAIL_PORT || 587,
+//       secure: false,
+//       auth: {
+//         user: process.env.MAIL_USER,
+//         pass: process.env.MAIL_PASS,
+//       },
+//     });
+
+//     const info = await transporter.sendMail({
+//       from: "StudyNotion Team <starktony1047@gmail.com>", // VERIFIED in Brevo
+//       to: email,
+//       subject: title,
+//       html: body,
+//     });
+
+//     console.log("MAIL SENT:", info.messageId);
+//     return info;
+//   } catch (error) {
+//     console.error("MAIL ERROR:", error);
+//   }
+// };
+
+// module.exports = mailsender;
+
+
+
 const nodemailer = require("nodemailer");
+
+const transporter = nodemailer.createTransport({
+  host: process.env.MAIL_HOST,
+  port: process.env.MAIL_PORT || 587,
+  secure: false,
+  auth: {
+    user: process.env.MAIL_USER,
+    pass: process.env.MAIL_PASS,
+  },
+});
 
 const mailsender = async (email, title, body) => {
   try {
-    const transporter = nodemailer.createTransport({
-      host: process.env.MAIL_HOST,
-      port: process.env.MAIL_PORT || 587,
-      secure: false,
-      auth: {
-        user: process.env.MAIL_USER,
-        pass: process.env.MAIL_PASS,
-      },
-    });
-
     const info = await transporter.sendMail({
-      from: "StudyNotion Team <starktony1047@gmail.com>", // VERIFIED in Brevo
+      from: "StudyNotion Team <starktony1047@gmail.com>",
       to: email,
       subject: title,
       html: body,
